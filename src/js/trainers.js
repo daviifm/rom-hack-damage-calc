835c96edb22159edd44a250382ddd88259d3ac8f
Davi Mint <davibirds@gmail.com>
2026-02-26 11:25:59 -0300

    Terminando mudanças visuais e começando treinadores

---
+++ src/js/trainers.js
@@ -0,0 +1,45 @@
+const trainers = [
+    {
+        name: "Cool Trainer Claude",
+        party: [
+            {
+            name: "Abomasnow",
+            item: "Heavy-Duty Boots",
+            level: 100,
+            ability: "Snow Warning",
+            nature: "Bold",
+            moves: ["Blizzard", "Wood Hammer", "Icy Wind", "Ice Shard"]
+            },
+            {
+            name: "Accelgor",
+            item: "Focus Sash",
+            level: 100,
+            ability: "Unburden",
+            nature: "Naive",
+            moves: ["Final Gambit", "Bug Buzz", "U-Turn", "First Impression"]
+            }
+        ]
+    },
+    {
+        name: "Ace Trainer Diamond",
+        party: [
+            {
+            name: "Weavile",
+            item: "Heavy-Duty Boots",
+            level: 100,
+            ability: "Pressure",
+            nature: "Adamant",
+            moves: ["Ice Spinner", "Ice Shard", "Night Slash", "Brick Break"]
+            },
+            {
+            name: "Rattata",
+            item: "Focus Sash",
+            level: 100,
+            ability: "Guts",
+            nature: "Naive",
+            moves: ["Endeavor", "Quick Attack", "Hyper Fang", "Crunch"]
+            }
+        ]
+    }
+]
+
